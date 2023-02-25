61b14c1636f6ff86cbb6bd897004bf4120851fc6
Björn Petersen <git@bjoernpetersen.net>
2023-02-25 19:54:59 +0100

    Fix linter warnings

--- lib/main_local.dart
+++ lib/main_local.dart
@@ -2,7 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:flutter/material.dart';
 
-import 'app.dart';
+import 'package:frontend/app.dart';
 
 void main() {
   Bloc.transformer = sequential();
